c29c88a4d536088fef9c260578108503d042a130
jwanner <jw0469a@student.american.edu>
2015-05-19 00:36:15 +0000

    config git

--- pizza.js
+++ pizza.js
@@ -2,5 +2,6 @@
     var person = prompt("How many people?", "Enter a number");
     var preorder = person * (3/8);
         document.getElementById("result").innerHTML =
+        //round up
           Math.ceil(preorder);
 }
