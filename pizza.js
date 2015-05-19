42652da6ac0bb76fa9a68135cec933cb1583c487
jwanner1 <jw0469a@american.edu>
2015-05-19 00:10:18 +0000

    cleaning things up

--- pizza.js
+++ pizza.js
@@ -1,35 +1,6 @@
-
-
-
-// var myButton = document.getElementById("clickButton");
-// var myText = document.getElementById("helloText");
-
-// myButton.addEventListener('click', doSomething, false);
-
-
 	function myFunction() {
     var person = prompt("How many people?", "Enter a number");
-    
     var preorder = person * (3/8);
-    var remainder = preorder % 1;
-    
-
-
-
-
-		
-		
         document.getElementById("result").innerHTML =
           Math.ceil(preorder);
-
 }
-	
-	
-	
-	// var preorder = guests * (3/8);
-	// var remainder = preorder % 1;
-	// if (preorder != 0)  
-	// 	order = (1-remainder)+(preorder-remainder)
-	// else
-	// 	order = preorder
-// 
